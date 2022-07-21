d998be2fe1439dc53c121d5a6f41b14bbeb9304f
Sergey <sp27101971@gmail.com>
2022-07-21 21:23:27 +0300

    CheckPalindrom

---
+++ Task-19/Program.cs
@@ -0,0 +1,29 @@
+﻿
+/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Enter five-digit number");
+int fiveDigit = int.Parse(Console.ReadLine());
+
+checkPalindrom(fiveDigit);
+
+
+void checkPalindrom(int fiveDig)
+{
+    int first = fiveDig / 10000;
+    int second = fiveDig / 1000 % 10;
+    int fifth = fiveDig % 10;
+    int fourth = fiveDig % 100 / 10;
+    
+    if (first == fifth && second == fourth)
+    {
+        Console.WriteLine("It's a palindrom");
+    }
+    else
+    {
+        Console.WriteLine("Nope) Try one more time");
+    }
+}
