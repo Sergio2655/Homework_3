1b3278081ae0f2b2c5091b7b6644e12571c75368
Sergey <sp27101971@gmail.com>
2022-07-21 21:27:25 +0300

    Cube

---
+++ Task-23/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Enter your number:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Enjoy:");
+int count = 1;
+int cube = count;
+while(count <= n)
+{
+    cube = count * count * count;
+    Console.WriteLine(cube);
+    count++;
+}
